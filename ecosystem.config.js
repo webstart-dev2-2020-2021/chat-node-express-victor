50304c973819f7b4609bb451682a02545cb5b0ae
Viezas <dxviezas@gmail.com>
2021-02-01 10:54:08 +0100

    test ignore du --watch sur inscription user

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     script: "app.js",
     watch: ["server", "client"],
-    ignore_watch : ["node_modules", "migrations/20201110193817-create-user.js"],
+    ignore_watch : ["node_modules", "./migrations/"],
     watch_options: {
       "followSymlinks": false
     }
